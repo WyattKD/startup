4e0f5ff77d975c224f80046d4a388859617de6bb
WyattKD <wykd47@gmail.com>
2025-02-06 11:06:22 -0700

    imported fonts, added navlinks and routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Hangman } from './hangman/hangman';
+import { Scores } from './scores/scores';
+import { Room_Settings } from './room_settings/room_settings';
+
 
 export default function App() {
     return (
-        <div className='app bg-dark text-light'>App will display here
-            <header>
-                <nav>
-                    <menu>
-                        <li><a href="login.html">Login</a></li>
-                        <li><a href="hangman.html">Hangman</a></li>
-                        <li><a href="scores.html">High Scores</a></li>
-                        <li><a href="room_settings.html">Game Settings</a></li>
-                    </menu>
-                </nav>
-            </header>
-            <footer>
-                <div class="text-reset">Wyatt Dunlap</div>
-                <a href="https://github.com/WyattKD/startup">GitHub</a>
-            </footer>
-        </div>
+        <BrowserRouter>
+            <div>
+                <header>
+                    <nav>
+                        <menu>
+                            <li><NavLink className='nav-link' to='login'>Login</NavLink></li>
+                            <li><NavLink className='nav-link' to='hangman'>Hangman</NavLink></li>
+                            <li><NavLink className='nav-link' to='scores'>High Scores</NavLink></li>
+                            <li><NavLink className='nav-link' to='room_settings'>Game Settings</NavLink></li>
+                        </menu>
+                    </nav>
+                </header>
+                <footer>
+                    <div class="text-reset">Wyatt Dunlap</div>
+                    <a href="https://github.com/WyattKD/startup">GitHub</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
---
+++ src/room_settings/room_settings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Room_Settings() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>login displayed here</div>
+    </main>
+  );
+}
