c76bd79234928a922c643667e02df6b4f624efcc
WyattKD <wykd47@gmail.com>
2025-02-25 15:41:18 -0700

    finished input word validation and logging in functionality

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,6 +5,8 @@ import './login.css';
 export function Login() {
   const [loginForm, setLoginForm] = React.useState({username:'', password:'', roomNumber:''})
   const [typeOfLogin, setTypeOfLogin] = React.useState('invalid')
+  const [bad_password, set_bad_password] = React.useState(false)
+  const [bad_entry, set_bad_entry] = React.useState(false)
   const navigate = useNavigate();
 
   function handleSubmit(loginStatus) {
@@ -20,9 +22,9 @@ export function Login() {
       localStorage.setItem("scores", "")
       navigate('/room_settings')
     } else if (loginStatus == "incorrectLogin") {
-      console.log("Bad Password >:(")
+      set_bad_password(true)
     } else {
-      console.log("Please fill out all entries!")
+      set_bad_entry(true)
     }
   }
 
@@ -44,15 +46,15 @@ export function Login() {
       <h2 className="login-h2">with a friend!</h2>
         <div className="mb-3">
           <label className="login-label">Username: </label>
-          <input onChange={e => {setLoginForm({...loginForm, username: e.target.value})}} type="username" className="login-input form-control" id="exampleFormControlInput1" placeholder="Enter your username" ></input>
+          <input onChange={e => {setLoginForm({...loginForm, username: e.target.value})}} type="username" className="login-input form-control" id="exampleFormControlInput1" placeholder={bad_entry ? "Please fill out all fields!" : "Enter your username"} ></input>
         </div>
           <label className="login-label">Password: </label>
-          <input onChange={e => {setLoginForm({...loginForm, password: e.target.value})}} type="password" id="inputPassword5" className="login-input form-control" placeholder="Enter your password"></input>
+          <input onChange={e => {setLoginForm({...loginForm, password: e.target.value})}} type="password" id="inputPassword5" className="login-input form-control" placeholder={bad_entry ? "Please fill out all fields!" : "Enter your password"}></input>
         <div className="mb-3">
           <label className="login-label">Room Number: </label>
-          <input onChange={e => {setLoginForm({...loginForm, roomNumber: e.target.value})}} type="number" className="login-input form-control" id="exampleFormControlInput2" placeholder="Enter the room number"></input>
+          <input onChange={e => {setLoginForm({...loginForm, roomNumber: e.target.value})}} type="number" className="login-input form-control" id="exampleFormControlInput2" placeholder={bad_entry ? "Please fill out all fields!" : "Enter your room number"}></input>
         </div>
-        <div className="form-text" id="basic-addon4">Enter the same room number as your friend!</div>
+        <div className="form-text" id="basic-addon4">{bad_password ? "Wrong password!" : "Enter the same room number as your friend!"}</div>
         <button onClick={() => handleSubmit(typeOfLogin)} type="submit" className="btn btn-primary login-button">Submit</button>
     </div>
   );
--- src/room_settings/input_word.jsx
+++ src/room_settings/input_word.jsx
@@ -5,12 +5,16 @@ import './input_word.css';
 export function Input_Word() {
   const navigate = useNavigate();
   const [theWord, setTheWord] = React.useState('')
-
+  const [valid_word, set_valid_word] = React.useState(true)
   function submit_word(key="Enter", event=null) {
-    if (key == "Enter" && theWord != "") {
+    if (key == "Enter" && theWord != "" && /^[a-zA-Z]+$/.test(theWord)) {
       localStorage.setItem("theWord", theWord)
       navigate('/hangman')
-    } 
+    } else if (key == "Enter") {
+      console.log("happened")
+      event.target.value = ""
+      set_valid_word(false)
+    }
   }
   return (
     <main className="word-main">
@@ -18,8 +22,8 @@ export function Input_Word() {
       
         <div className="mb-3">
           <h1 className="word-h1">Enter Word: </h1>
-          <input onKeyDown={e => submit_word(e.key)} onChange={e => {setTheWord(e.target.value.trim())}} type="text" className="form-control word-input" id="exampleFormControlInput2" maxLength="30"></input>
-          <button onClick={() => submit_word()} type="submit" className="btn btn-success word-button">Confirm</button>
+          <input onKeyDown={e => submit_word(e.key, e)} onChange={e => {setTheWord(e.target.value.trim())}} type="text" className="form-control word-input" id="exampleFormControlInput2" maxLength="30" placeholder={valid_word ? "" : "Not a valid word!"}></input>
+          <button onClick={e => submit_word("Enter", e)} type="submit" className="btn btn-success word-button">Confirm</button>
         </div>
       </div>
     </main>
