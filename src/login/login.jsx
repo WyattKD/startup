1ddd4a0942c05ae68af960150b98b44609f91404
WyattKD <wykd47@gmail.com>
2025-03-06 15:34:04 -0700

    more cleaning

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,26 +3,24 @@ import { useNavigate } from 'react-router-dom';
 import './login.css';
 
 export function Login() {
-  const [loginForm, setLoginForm] = React.useState({username:'', password:'', roomNumber:''})
-  const [typeOfLogin, setTypeOfLogin] = React.useState('invalid')
-  const [bad_entry, set_bad_entry] = React.useState(false)
+  const [login_form, set_login_form] = React.useState({username:'', password:'', roomNumber:''})
   const [error_message, set_error_message] = React.useState("Enter the same room number as your friend!")
   const navigate = useNavigate();
 
   async function login() {
-    if (loginForm.username == "" || loginForm.password == "" || loginForm.roomNumber == "") {
+    if (login_form.username == "" || login_form.password == "" || login_form.roomNumber == "") {
       set_error_message("Error: Please fill out all fields!")
     } else {
       const response = await fetch(`/api/auth/login`, {
         method: 'post',
-        body: JSON.stringify({ user_name: loginForm.username, password: loginForm.password }),
+        body: JSON.stringify({ user_name: login_form.username, password: login_form.password }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
       });
       if (response?.status === 200) {
-        localStorage.setItem("currentUser", loginForm.username)
-        localStorage.setItem("currentRoomNumber", loginForm.roomNumber)
+        localStorage.setItem("currentUser", login_form.username)
+        localStorage.setItem("currentRoomNumber", login_form.roomNumber)
         localStorage.setItem("scores", "")
         navigate('/room_settings')
       } else if (response?.status === 401) {
@@ -34,19 +32,19 @@ export function Login() {
   }
 
   async function sign_up() {
-    if (loginForm.username == "" || loginForm.password == "" || loginForm.roomNumber == "") {
+    if (login_form.username == "" || login_form.password == "" || login_form.roomNumber == "") {
       set_error_message("Error: Please fill out all fields!")
     } else {
       const response = await fetch(`/api/auth/sign_up`, {
         method: 'post',
-        body: JSON.stringify({ user_name: loginForm.username, password: loginForm.password }),
+        body: JSON.stringify({ user_name: login_form.username, password: login_form.password }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
       });
       if (response?.status === 200) {
-        localStorage.setItem("currentUser", loginForm.username)
-        localStorage.setItem("currentRoomNumber", loginForm.roomNumber)
+        localStorage.setItem("currentUser", login_form.username)
+        localStorage.setItem("currentRoomNumber", login_form.roomNumber)
         localStorage.setItem("scores", "")
         navigate('/room_settings')
       } else if(response?.status === 409) {
@@ -63,13 +61,13 @@ export function Login() {
       <h2 className="login-h2">with a friend!</h2>
         <div className="mb-3">
           <label className="login-label">Username: </label>
-          <input autoComplete="off" onKeyDown={e => {if (e.key=="Enter") {login()}}} onChange={e => {setLoginForm({...loginForm, username: e.target.value})}} type="username" className="login-input form-control" id="exampleFormControlInput1" placeholder={bad_entry ? "Please fill out all fields!" : "Enter your username"} ></input>
+          <input autoComplete="off" onKeyDown={e => {if (e.key=="Enter") {login()}}} onChange={e => {set_login_form({...login_form, username: e.target.value})}} type="username" className="login-input form-control" id="exampleFormControlInput1" placeholder="Enter your username" ></input>
         </div>
           <label className="login-label">Password: </label>
-          <input autoComplete="off" onKeyDown={e => {if (e.key=="Enter") {login()}}} onChange={e => {setLoginForm({...loginForm, password: e.target.value})}} type="password" id="inputPassword5" className="login-input form-control" placeholder={bad_entry ? "Please fill out all fields!" : "Enter your password"}></input>
+          <input autoComplete="off" onKeyDown={e => {if (e.key=="Enter") {login()}}} onChange={e => {set_login_form({...login_form, password: e.target.value})}} type="password" id="inputPassword5" className="login-input form-control" placeholder="Enter your password"></input>
         <div className="mb-3">
           <label className="login-label">Room Number: </label>
-          <input autoComplete="off" onKeyDown={e => {if (e.key=="Enter") {login()}}} onChange={e => {setLoginForm({...loginForm, roomNumber: e.target.value})}} type="number" className="login-input form-control" id="exampleFormControlInput2" placeholder={bad_entry ? "Please fill out all fields!" : "Enter your room number"}></input>
+          <input autoComplete="off" onKeyDown={e => {if (e.key=="Enter") {login()}}} onChange={e => {set_login_form({...login_form, roomNumber: e.target.value})}} type="number" className="login-input form-control" id="exampleFormControlInput2" placeholder="Enter your room number"></input>
         </div>
         <div className="form-text" id="basic-addon4">{error_message}</div>
         <button onClick={() => login()} type="login" className="btn btn-primary login-button">Login</button>
